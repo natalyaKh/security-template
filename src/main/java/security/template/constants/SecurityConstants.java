35a6f9da562d4b31041f2682f245bbac1cf102f1
natalyaKh <khrebtiivski@gmail.com>
2021-06-03 13:44:22 +0300

    add spring security

---
+++ src/main/java/security/template/constants/SecurityConstants.java
@@ -0,0 +1,7 @@
+package security.template.constants;
+
+
+public class SecurityConstants {
+    public static final String SIGN_UP_URL = "/users/v1";
+    public static final String ACCESS_TO_DB = "/h2-console/**";
+}
