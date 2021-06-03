cebe350ca5bfafa450494d3f705ca97290538cce
natalyaKh <khrebtiivski@gmail.com>
2021-06-03 12:15:24 +0300

    init project

---
+++ src/main/java/security.template/SecurityTemplateApplication.java
@@ -0,0 +1,14 @@
+package security.template;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityTemplateApplication.class, args);
+    }
+
+}
