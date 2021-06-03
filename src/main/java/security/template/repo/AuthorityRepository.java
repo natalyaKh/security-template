c7d06d0dd6147b12da28d02b32f683c0b166e0b1
natalyaKh <khrebtiivski@gmail.com>
2021-06-03 17:17:37 +0300

    add roles and authorities

---
+++ src/main/java/security/template/repo/AuthorityRepository.java
@@ -0,0 +1,8 @@
+package security.template.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import security.template.models.AuthorityEntity;
+import security.template.models.RoleEntity;
+
+public interface AuthorityRepository extends JpaRepository<AuthorityEntity, Long> {
+}
