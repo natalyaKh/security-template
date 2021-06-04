67e122031a0cecb4c803387c71d916f749926403
natalyaKh <khrebtiivski@gmail.com>
2021-06-04 16:02:40 +0300

    secure other methods

--- src/main/java/security/template/controllers/UserController.java
+++ src/main/java/security/template/controllers/UserController.java
@@ -1,6 +1,5 @@
 package security.template.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -21,35 +20,38 @@ public class UserController {
     }
 
     @PostMapping()
-    public ResponseEntity register(@RequestBody UserDto userDto){
+    public ResponseEntity register(@RequestBody UserDto userDto) {
         return new ResponseEntity(
             userService.createUser(userDto), HttpStatus.CREATED);
     }
 
     @PostMapping("/test")
-    public String test(){
+    public String test() {
         return "I am work: Test method";
     }
 
+    @PreAuthorize("hasAnyRole('SUPER_ADMIN', 'ADMIN')")
     @GetMapping()
-    public ResponseEntity getAllUsers(){
+    public ResponseEntity getAllUsers() {
         return new ResponseEntity(
             userService.getAllUsers(), HttpStatus.OK
         );
     }
 
+    @PreAuthorize("hasAnyRole('SUPER_ADMIN', 'ADMIN') or #userEmail == principal.id")
     @GetMapping("/{userEmail}")
-    public ResponseEntity getUserByUserUuid(@PathVariable String userEmail){
+    public ResponseEntity getUserByUserUuid(@PathVariable String userEmail) {
         return new ResponseEntity(
             userService.getUserByUserUuid(userEmail), HttpStatus.OK
         );
     }
 
-//    TODO write normal code with DTO and one parameter
+    //    TODO write normal code with DTO and one parameter
+    @PreAuthorize("hasRole('SUPER_ADMIN') or  hasAnyRole('ADMIN', 'USER') and #userEmail == principal.id")
     @PutMapping("put/{userEmail}/{firstName}/{lastName}")
     public ResponseEntity updateUserByUserUuid(@PathVariable String userEmail,
                                                @PathVariable String firstName,
-                                               @PathVariable String lastName){
+                                               @PathVariable String lastName) {
         return new ResponseEntity(
             userService.updateUserByUserUuid(userEmail, firstName, lastName), HttpStatus.OK
         );
@@ -57,9 +59,9 @@ public class UserController {
 
     @PreAuthorize("hasRole('SUPER_ADMIN') or hasRole('ADMIN') and #userEmail == principal.id")
     @DeleteMapping("/{userEmail}")
-    public ResponseEntity deleteUserByUserUuid(@PathVariable String userEmail){
+    public ResponseEntity deleteUserByUserUuid(@PathVariable String userEmail) {
         boolean deleted = userService.deleteUserByUserUuid(userEmail);
-        if(deleted){
+        if (deleted) {
             return new ResponseEntity(
                 Status.SUCCESSFUL, HttpStatus.OK
             );
