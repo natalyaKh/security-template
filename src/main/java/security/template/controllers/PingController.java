28e41bb901b62d83053e74e7a56de70354deee90
natalyaKh <54761439+natalyaKh@users.noreply.github.com>
2021-06-04 16:58:43 +0300

    secure ping controller (#18)

--- src/main/java/security/template/controllers/PingController.java
+++ src/main/java/security/template/controllers/PingController.java
@@ -1,14 +1,19 @@
 package security.template.controllers;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import security.template.constants.SecurityConstants;
 
 @RestController
 @RequestMapping("/ping/v1")
+@PreAuthorize("hasRole('SUPER_ADMIN')")
+//@PreAuthorize("isAuthenticated()")
 public class PingController {
+
     @GetMapping()
-    public String ping(){
-        return "I am work. Access only for SuperAdministrator";
+    public String pingAdmin() {
+        return "I am work on port: " + SecurityConstants.getPort();
     }
 }
